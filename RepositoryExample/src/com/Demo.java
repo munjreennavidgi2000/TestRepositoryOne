1f472c6ca1d4f79b9693b03ecdf177ac30b3e82e
munjreennavidgi2000 <128956054+munjreennavidgi2000@users.noreply.github.com>
2023-11-24 21:40:16 +0530

    Update Demo.java

--- RepositoryExample/src/com/Demo.java
+++ RepositoryExample/src/com/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
  public static void main(String[] args) {
 	System.out.println("Demo repository example");
 	System.out.println("insertion completed");
+	 System.out.println("changes done by author");
 }
 }
